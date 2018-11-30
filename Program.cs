ec16e19c2b4dea2d4f35a7c8d2bf4d382a9e6c11
Gregory Smith <gregsmith446@gmail.com>
2018-11-30 17:15:29 -0500

    basic for each loop

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main()
+        {
+            // for each loop example
+
+            // start by creating an array of ints as such
+            int[] numbers = { 4, 5, 6, 7, 8, 9 };
+
+            // loop through the array one number at a time
+            foreach (int num in numbers)
+            {
+                Console.WriteLine("{0} ", num); // write each number to the console
+            }
+
+
+        }
+    }
+}
